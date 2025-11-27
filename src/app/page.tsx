ac99dab2c983a430a86b146fb19aaee7d420d676
Ritikkr44 <93464370+Ritikkr44@users.noreply.github.com>
2025-11-27 12:38:37 +0530

    Best Match UI is completed working on the requirements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,120 @@
 import Image from "next/image";
+import { CiCalendarDate } from "react-icons/ci";
+import { FaFlag, FaHeart, FaStar, FaWallet } from "react-icons/fa";
+import { bestMatchData } from "../../data/bestMatch";
+import { TiTick } from "react-icons/ti";
+
 
 export default function Home() {
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+
+    <><h1 className="font-bold">Best Match</h1>
+      {bestMatchData.map((object, index) => <div key={index} className="mt-10 hover:shadow-xl transition duration-300 rounded-2xl">
+        <div className="border border-gray-300 flex">
+
+          <div className="size-96 overflow-hidden relative">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src={object.imageSrc}
+              alt="Trip Image"
+              fill
+              className="object-cover"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+            <div className="absolute  right-2 top-2 w-10 h-10 bg-black/50 rounded-full flex justify-center items-center cursor-pointer">
+              <FaHeart className="text-white " />
+            </div>
+          </div>
+
+
+
+
+          <div className="w-full py-5 px-5">
+            <div className="flex">
+              <div className="basis-full">
+                <p className="font-bold text-2xl">{object.information.heading}</p>
+                <p>{object.information.description}</p>
+
+                <div className="activities text-blue-500 flex text-center mt-4 gap-3">
+
+                  {object.information.activities.map((activity, index) =>
+                    <p key={index} className=" bg-blue-200 w-fit rounded-md px-3">{activity}</p>
+                  )}
+                </div>
+
+                <div className="destinationCircle flex mt-5 gap-3">
+
+                  <div className="circle">
+                    <p className="w-4 h-4 border rounded-full "></p>
+                    <p className="border-l-2 border-dashed border-gray-800 h-8 ml-1.5"></p>
+                    <p className="w-4 h-4 border bg-black rounded-full "></p>
+                  </div>
+                  <div className="destination">
+                    <p className="pb-4">{object.information.fromLocation}</p>
+                    <p>{object.information.toLocation}</p>
+                  </div>
+
+                </div>
+
+                <div className="datePriceTrip flex gap-15 mt-4">
+                  <p className="flex items-center gap-2"> <CiCalendarDate />{object.information.date}</p>
+                  <p className="flex items-center gap-2"> <FaWallet />	&#8377;{object.information.pricePerPerson} / person</p>
+                  <p className="flex items-center gap-2"><FaFlag />{object.information.count} Trips Completed</p>
+                </div>
+              </div>
+              <div className="flex text-center gap-4 h-auto">
+                <p className="bg-green-500 h-10 w-30 text-white font-bold rounded-4xl pt-2">{object.information.matchPercentage}% Match</p>
+                <p className="bg-red-500 h-10 w-30 text-white font-bold rounded-4xl pt-2">{object.information.spotsAvailable} Spots left</p>
+
+              </div>
+            </div>
+            <hr className="my-4" />
+
+            <div className="flex  items-center">
+              <div className="userProfile flex gap-4 items-center basis-1/2">
+
+                <div className="profilePic text-white size-13 text-2xl font-bold bg-blue-500 rounded-full flex justify-center items-center">
+                  <p>SJ</p>
+                </div>
+
+                <div className="namePlaceName">
+                  <div className="flex gap-1.5 items-center">
+                    <p className="name font-bold">Sarah Johnson,</p>
+                    <p className="age font-bold">18</p>
+                    <div className="bg-green-400 flex h-5 w-5 justify-center items-center rounded-full text-white">
+                      <TiTick />
+                    </div>
+                  </div>
+
+                  <div className="flex gap-3 items-center">
+                    <p className="stateName">Goa,</p>
+                    <p className="countryName">India.</p>
+                    <FaStar className="text-amber-400" />
+                  </div>
+
+
+                  <div className="flex gap-4 mt-2">
+                    <p className=" bg-blue-100 rounded-lg px-2 text-blue-500 font-semibold">Travel Enthusiast</p>
+                    <p className="border-2 border-green-300 bg-green-100 rounded-lg px-2 text-green-600 font-semibold">90% Safe</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="basis-1/2 gap-4 flex">
+              <button className="px-3 py-1 bg-green-950 text-white h-fit cursor-pointer">View Trip</button>
+              <button className="px-3 py-1 bg-green-950 text-white h-fit cursor-pointer">Join Trip</button>
+              <button className="px-3 py-1 bg-green-950 text-white h-fit cursor-pointer">View Profile</button>
+              </div>
+            </div>
+
+          </div>
+
+
         </div>
-      </main>
-    </div>
-  );
+
+      </div>
+      )}
+    </>
+
+  )
 }
